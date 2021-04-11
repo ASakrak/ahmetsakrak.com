3a1a5b7a00f373e1e3a8b1247838ac3ffce07dd9
LnxyBTW <TTV_BTW73498@outlook.com>
2021-04-11 15:55:44 +0300

    test
    test

---
+++ backgroundAnimation.js
@@ -0,0 +1,20 @@
+function createSquare() {
+    const section = document.querySelector('section');
+    const square = document.createElement('span');
+
+
+    var size = Math.random() * 50;
+
+    square.style.width = 20 + size + 'px';
+    square.style.height = 20 + size + 'px';
+
+    square.style.top = Math.random() * innerHeight + 'px';
+    square.style.left = Math.random() * innerWidth + 'px';
+
+    section.appendChild(square);
+
+    setTimeout(() => {
+        square.remove()
+    }, 5000)
+}
+setInterval(createSquare, 150)
