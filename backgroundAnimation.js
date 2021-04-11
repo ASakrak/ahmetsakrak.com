0a1a102d3c8a16b03d21907b5e07bdb994e1cdfd
LnxyBTW <TTV_BTW73498@outlook.com>
2021-04-11 16:03:32 +0300

    test123
    123

--- backgroundAnimation.js
+++ backgroundAnimation.js
@@ -3,10 +3,10 @@ function createSquare() {
     const square = document.createElement('span');
 
 
-    var size = Math.random() * 50;
+    var size = Math.random() * 10;
 
-    square.style.width = 20 + size + 'px';
-    square.style.height = 20 + size + 'px';
+    square.style.width = 5 + size + 'px';
+    square.style.height = 5 + size + 'px';
 
     square.style.top = Math.random() * innerHeight + 'px';
     square.style.left = Math.random() * innerWidth + 'px';
