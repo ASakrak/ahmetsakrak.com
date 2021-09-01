65cd75e4901a776e33912810c66e4231e62d4b7f
LnxyBTW <TTV_BTW73498@outlook.com>
2021-09-01 17:18:14 +0300

    update

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function typeWriterEffects() {
 
     //değişkenler
 
-    const words = ["Ben Ahmet Şakrak Bloguma Hoşgeldiniz.", "Ben Ahmet Şakrak Software Developerım.", "Ben Ahmet Şakrak"]
+    const words = ["I'm Ahmet Şakrak welcome to my blog.", "I'm Ahmet Şakrak"]
 
     let wordCount = 0;
 
