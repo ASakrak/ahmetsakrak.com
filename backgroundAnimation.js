9cc71e7ef6437f90e045251c4aac6da6ac6a8665
LnxyBTW <TTV_BTW73498@outlook.com>
2022-08-30 00:32:55 +0300

    Update backgroundAnimation.js

--- backgroundAnimation.js
+++ backgroundAnimation.js
@@ -5,9 +5,9 @@ function createSquare() {
 
     var size = Math.random() * 10;
 
-    square.style.width = 55 + 'px';
-    square.style.height = 55 + 'px';
-
+    square.style.width = 70 + 'px';
+    square.style.height = 70 + 'px';
+    //55/55
     square.style.top = Math.random() * innerHeight + 'px';
     square.style.left = Math.random() * innerWidth + 'px';
 
