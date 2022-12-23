b3f540cb0d9c51c7a7456eb787b3e7ed7cba3c50
LnxyBTW <TTV_BTW73498@outlook.com>
2022-12-23 20:40:28 +0300

    update

--- backgroundAnimation.js
+++ backgroundAnimation.js
@@ -1,12 +1,12 @@
-/*function createSquare() {
+function createSquare() {
     const section = document.querySelector('section');
     const square = document.createElement('span');
 
 
     var size = Math.random() * 10;
 
-    square.style.width = 55 + 'px';
-    square.style.height = 55 + 'px';
+    square.style.width = 70 + 'px';
+    square.style.height = 70 + 'px';
     //55/55
     square.style.top = Math.random() * innerHeight + 'px';
     square.style.left = Math.random() * innerWidth + 'px';
@@ -17,4 +17,4 @@
         square.remove()
     }, 5000)
 }
-setInterval(createSquare, 100)*/
+setInterval(createSquare, 100)
